135d75412c33a24128f3bc44270a6a27127a2897
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-02 14:04:31 +0600

    add some code

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -3,13 +3,14 @@ import axios from "axios";
 import React, { useState } from "react";
 
 export default function AddProduct() {
-  const [state, setState] = useState({
+  const initialState = {
     title: "",
     description: "",
-    price: 0,
+    price: "",
     rating: 5,
     thumbnail: "",
-  });
+  };
+  const [state, setState] = useState(initialState);
 
   const handleChange = (e) => {
     setState((prevState) => {
@@ -19,12 +20,94 @@ export default function AddProduct() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setState(initialState);
+    console.log(state);
   };
 
   return (
-    <div className=" m-2 p-2 bg-gray-200 w-1/5 h-1/2">
+    <div className=" m-2 p-2  w-1/5 h-1/2">
       <h1 className=" text-3xl my-2">Add a Product</h1>
-      <form className=" flex flex-col" onSubmit={handleSubmit}>
+
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Title
+          </label>
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Product Title"
+            required
+            name="title"
+            value={state.title}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Description
+          </label>
+          <textarea
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Product Title"
+            name="description"
+            value={state.description}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="price"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Price
+          </label>
+          <input
+            type="number"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter Product Title"
+            required
+            name="price"
+            value={state.price}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="thumbnail"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Thumbnail
+          </label>
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Thumbnail...."
+            name="thumbnail"
+            value={state.thumbnail}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Add Product
+        </button>
+      </form>
+
+      {/* <form className=" flex flex-col max-w-sm mx-auto" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter a Product Name..."
@@ -48,7 +131,7 @@ export default function AddProduct() {
           type="number"
           placeholder="Enter Product Price..."
           name="price"
-          value={state.price}
+          value={state.price === 0 ? "" : state.price}
           onChange={handleChange}
         />
         <br />
@@ -61,7 +144,14 @@ export default function AddProduct() {
           onChange={handleChange}
         />
         <br />
-      </form>
+
+        <button
+          type="submit"
+          className=" bg-black m-auto p-2 rounded-lg text-white text-xl"
+        >
+          Add Product
+        </button>
+      </form> */}
     </div>
   );
 }
