ce2a8195dd0a0bde2486333fd9e38f3e5e09f98e
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-02 07:01:29 +0600

    add some code

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -27,12 +27,12 @@ export default function ProductsList() {
   }
   return (
     <div className=" flex flex-col justify-center items-center w-3/5">
-      <h1>All Prodcts</h1>
-      <ul>
+      <h1 className=" text-3xl font-bold">All Prodcts</h1>
+      <ul className=" flex flex-wrap justify-center items-center">
         {products?.map((product) => (
-          <li key={product.id}>
+          <li className=" flex flex-col items-center m-2 border rounded-sm" key={product.id}>
             <p>{product.title}</p>
-            <img src={product.thumbnail} alt="product image" />
+            <img className=" object-cover h-64 w-96 rounded-sm" src={product.thumbnail} alt="product image" />
           </li>
         ))}
       </ul>
