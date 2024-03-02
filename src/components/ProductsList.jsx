2f9f158640776d0f1a7b8ceedb9c79b67b4beb41
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-02 06:29:31 +0600

    add some code

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+
+const retrieveProducts = async () => {
+  const response = await axios.get("http://localhost:3000/products");
+  const data = response.data;
+  return data;
+};
 
 export default function ProductsList() {
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: retrieveProducts,
+  });
+
+  if (isLoading) {
+    return <div> Fetching Data.....</div>;
+  }
+
+  if (error) {
+    return <div>Error while fetching data {error.message}</div>;
+  }
   return (
-    <div>
-      Products
+    <div className=" flex flex-col justify-center items-center w-3/5">
+      <h1>All Prodcts</h1>
+      <ul>
+        {products?.map((product) => (
+          <li key={product.id}>
+            <p>{product.title}</p>
+            <img src={product.thumbnail} alt="product image" />
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
